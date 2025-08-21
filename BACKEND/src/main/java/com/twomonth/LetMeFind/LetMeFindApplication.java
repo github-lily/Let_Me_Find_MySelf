cae3732c44aaa331fc09cc5f80b7d76f8ba6cdd2
xoghd32 <xoghd32@naver.com>
2025-08-22 08:09:11 +0900

    feat:백엔드 셋팅

---
+++ BACKEND/src/main/java/com/twomonth/LetMeFind/LetMeFindApplication.java
@@ -0,0 +1,13 @@
+package com.twomonth.LetMeFind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetMeFindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetMeFindApplication.class, args);
+	}
+
+}
