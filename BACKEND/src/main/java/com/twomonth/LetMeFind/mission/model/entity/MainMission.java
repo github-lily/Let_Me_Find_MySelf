379062332248b14b9e71494f08ea71158cf85017
xoghd32 <xoghd32@naver.com>
2025-09-01 08:51:45 +0900

    feat:mainMission entity구현

---
+++ BACKEND/src/main/java/com/twomonth/LetMeFind/mission/model/entity/MainMission.java
@@ -0,0 +1,27 @@
+package com.twomonth.LetMeFind.mission.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "main_missions")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Builder
+public class MainMission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mission_id")
+    private Long id;
+
+    @Column(length = 250, nullable = false)
+    private String title;
+
+    @Column(length = 1000)
+    private String details;
+
+    @Column(length = 1000)
+    private String expectation;
+
+    @OneToMany(mappedBy = "mission")
+    private List<MainAssignment> assignments;
+}
