e01ad542926c99fe06981ecfd02c9bc9856b62f5
xoghd32 <xoghd32@naver.com>
2025-09-01 08:54:11 +0900

    feat:subMission entity구현

---
+++ BACKEND/src/main/java/com/twomonth/LetMeFind/mission/model/entity/SubMission.java
@@ -0,0 +1,21 @@
+package com.twomonth.LetMeFind.mission.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "sub_missions")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Builder
+public class SubMission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sub_mission_id")
+    private Long id;
+
+    @Column(length = 250, nullable = false)
+    private String title;
+
+    @OneToMany(mappedBy = "subMission")
+    private List<SubAssignment> assignments;
+}
