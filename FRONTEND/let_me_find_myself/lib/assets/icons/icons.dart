9185dd1365a0607550119dc6915667b75516d8d2
99minj0731 <99minj0731@naver.com>
2025-09-04 15:17:22 +0900

    Design: 하단바 아이콘 변경

---
+++ FRONTEND/let_me_find_myself/lib/assets/icons/icons.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  // 하단 바 사이즈 고정
+  static const double _navbarSize = 24.0;
+
+  // 네브바 요소들
+  static SvgPicture home({Color? color, double size = _navbarSize}) =>
+      SvgPicture.asset(
+        'lib/assets/icons/home.svg',
+        width: size,
+        height: size,
+        color: color,
+      );
+  static SvgPicture missionList({Color? color, double size = _navbarSize}) =>
+      SvgPicture.asset(
+        'lib/assets/icons/mission_list.svg',
+        width: size,
+        height: size,
+        color: color,
+      );
+  static SvgPicture report({Color? color, double size = _navbarSize}) =>
+      SvgPicture.asset(
+        'lib/assets/icons/report.svg',
+        width: size,
+        height: size,
+        color: color,
+      );
+  static SvgPicture calendar({Color? color, double size = _navbarSize}) =>
+      SvgPicture.asset(
+        'lib/assets/icons/calendar.svg',
+        width: size,
+        height: size,
+        color: color,
+      );
+  static SvgPicture myPage({Color? color, double size = _navbarSize}) =>
+      SvgPicture.asset(
+        'lib/assets/icons/user.svg',
+        width: size,
+        height: size,
+        color: color,
+      );
+}
--- FRONTEND/let_me_find_myself/lib/widgets/bottom_navbar.dart
+++ FRONTEND/let_me_find_myself/lib/widgets/bottom_navbar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:let_me_find_myself/assets/icons/icons.dart';
 
 class BottomNavbar extends StatelessWidget {
-
   final int currentIndex;
   final ValueChanged<int> onTab;
 
@@ -15,15 +15,42 @@ class BottomNavbar extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: currentIndex,
-        onTap: onTab,
+      onTap: onTab,
       type: BottomNavigationBarType.fixed,
       backgroundColor: Colors.white,
       items: [
-        BottomNavigationBarItem(icon: Icon(Icons.access_alarm), label: ''),
-        BottomNavigationBarItem(icon: Icon(Icons.access_alarm), label: ''),
-        BottomNavigationBarItem(icon: Icon(Icons.access_alarm), label: ''),
-        BottomNavigationBarItem(icon: Icon(Icons.access_alarm), label: ''),
-        BottomNavigationBarItem(icon: Icon(Icons.access_alarm), label: ''),
-  ]);
+        BottomNavigationBarItem(
+          icon: AppIcons.home(
+            color:  currentIndex == 0 ? Colors.blue : Colors.green,
+          ),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: AppIcons.missionList(
+            color:  currentIndex == 1 ? Colors.blue : Colors.green,
+          ),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: AppIcons.report(
+            color:  currentIndex == 2 ? Colors.blue : Colors.green,
+          ),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: AppIcons.calendar(
+            color:  currentIndex == 3 ? Colors.blue : Colors.green,
+          ),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: AppIcons.myPage(
+            color:  currentIndex == 4 ? Colors.blue : Colors.green,
+          ),
+          label: '',
+        ),
+
+      ],
+    );
   }
 }
